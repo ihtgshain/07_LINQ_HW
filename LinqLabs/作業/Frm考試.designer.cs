2aa724cd550a16fc94eef199caf4459061fb0293
ihtgshain <ihtgschen@gmail.com>
2022-05-05 10:50:16 +0800

    0505 誤字修改

--- LinqLabs/作業/Frm考試.designer.cs
+++ LinqLabs/作業/Frm考試.designer.cs
@@ -194,7 +194,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(375, 75);
             this.button1.TabIndex = 147;
-            this.button1.Text = "每個學生個人成績";
+            this.button1.Text = "每科成績統計";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
